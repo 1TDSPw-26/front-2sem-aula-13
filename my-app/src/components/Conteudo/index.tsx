bd12b81809f758f329b72099355426b5dfdd5a6c
murilloslopes <murillosivierolopes2310@gmail.com>
2026-09-22 19:52:04 -0300

    feat: merge

--- my-app/src/components/Conteudo/index.tsx
+++ my-app/src/components/Conteudo/index.tsx
@@ -6,41 +6,6 @@ export default function Conteudo() {
     <main>
       <section>
         <h2>Conteúdo</h2>
-<<<<<<< HEAD
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-          cupiditate debitis quaerat impedit dolor veniam dicta fugit.
-          Perferendis voluptas eos, unde quidem corporis mollitia quod sequi
-          eius, optio impedit magnam?
-        </p>
-
-        {/* Importando imagem externa */}
-
-        <figure>
-          <img
-            src="https://placehold.co/600x400/229999/000/png"
-            alt="Figure de demonstração"
-          />
-
-          <figcaption>Imagem externa</figcaption>
-        </figure>
-
-        {/* Importando imagem interna */}
-
-        <figure>
-          <img src={imgQuadrado} alt="Figure de demonstração" />
-
-          <figcaption>Imagem interna</figcaption>
-        </figure>
-
-        <figure>
-          <img src="/quadradoVermelho.png" alt="Figure de demonstração" />
-
-          <figcaption>Imagem estática</figcaption>
-        </figure>
-
-
-=======
 
             <Imagens/>
 
@@ -68,7 +33,6 @@ export default function Conteudo() {
           dolorem numquam ratione atque! Perspiciatis assumenda magnam neque
           omnis porro non sequi unde! Eius nihil error delectus et earum id quo.
         </p>
->>>>>>> feature/exemplo-pf0670
       </section>
     </main>
   );
